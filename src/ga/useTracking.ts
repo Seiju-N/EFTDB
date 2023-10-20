72beaf31cd9ff277c0c221d7fafb01c9db28310f
Seiju-N <seiju.nomura@gmail.com>
2023-10-20 17:23:53 +0900

    packages update

--- src/ga/useTracking.ts
+++ src/ga/useTracking.ts
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 
 declare global {
   interface Window {
-    gtag?: (key: string, trackingId: string, config?: { page_location?: string | undefined, debug_mode?: boolean }) => void;
+    gtag?: (key: string, trackingId: string, config?: { page_path?: string ,page_location?: string | undefined, debug_mode?: boolean }) => void;
   }
 }
 
@@ -12,13 +12,21 @@ export const useTracking = (trackingId: string | undefined = "G-93Z965NJ8Q") => 
 
   useEffect(() => {
     if (!window.gtag) return;
+    console.log('Sending data to Google Analytics');
     if (!trackingId) {
       console.log(
         'Tracking not enabled, as `trackingId` was not given and there is no `GA_MEASUREMENT_ID`.',
       );
       return;
     }
-    window.gtag('config', trackingId,);
-    window.gtag('event', '_page_view', { page_location : location.pathname })
+    const pagePath = `${window.location.origin}${location.pathname}${location.search}${location.hash}`;
+
+    window.gtag('config', trackingId, {
+      page_path: location.pathname,
+      page_location: pagePath,
+      debug_mode: true
+    });
+    
+    console.log(location.pathname)
   }, [location.pathname, trackingId]);
 };
