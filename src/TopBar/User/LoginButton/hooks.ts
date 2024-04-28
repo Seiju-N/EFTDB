181bccb327a9cbea6f049bc146b83cfd30742f27
Seiju-N <seiju.nomura@gmail.com>
2024-04-28 22:23:43 +0900

    Update Discord client ID and OAuth2 redirect URI in LoginButton/hooks.ts

--- src/TopBar/User/LoginButton/hooks.ts
+++ src/TopBar/User/LoginButton/hooks.ts
@@ -1,10 +1,10 @@
 import { useCallback } from "react";
 
-const REACT_APP_DISCORD_CLIENT_ID = process.env.DEVELOPMENT ? process.env.REACT_APP_DISCORD_CLIENT_ID : "1178560759064702976";
-const REACT_APP_OAUTH2_REDIRECT_URI = process.env.DEVELOPMENT ? process.env.REACT_APP_OAUTH2_REDIRECT_URI : "https://discord.com/api/oauth2/authorize";
+const REACT_APP_DISCORD_CLIENT_ID = process.env.REACT_APP_DEVELOPMENT ? process.env.REACT_APP_DISCORD_CLIENT_ID : "1178560759064702976";
+const REACT_APP_OAUTH2_REDIRECT_URI = process.env.REACT_APP_DEVELOPMENT ? process.env.REACT_APP_OAUTH2_REDIRECT_URI : "https%3A%2F%2Feftdb.info%2Fauth%2Fcallback";
 
 const _REDIRECT_URL =
-  `https://discord.com/oauth2/authorize?client_id=${REACT_APP_DISCORD_CLIENT_ID}&redirect_uri=${REACT_APP_OAUTH2_REDIRECT_URI}%2Fauth%2Fcallback&response_type=code&scope=identify%20guilds%20email`;
+  `https://discord.com/oauth2/authorize?client_id=${REACT_APP_DISCORD_CLIENT_ID}&redirect_uri=${REACT_APP_OAUTH2_REDIRECT_URI}&response_type=code&scope=identify%20guilds%20email`;
 
 export const useHooks = () => {
   const handleLogin = useCallback(() => {
